dbf6183029c60f2bf86d9f240e981948b5d4f133
William Yu <william@reitransfer.com>
2026-05-28 11:19:52 +0200

    v2: bigger hero top padding on mobile (pt-32 = 128px gap)

--- components/v2/Hero.tsx
+++ components/v2/Hero.tsx
@@ -32,7 +32,7 @@ export default function Hero({ marketName, phoneDisplay, phoneHref, serviceAreas
       <div className="absolute inset-0 bg-gradient-to-b from-black/80 via-black/65 to-black/90" aria-hidden />
       <div className="absolute inset-y-0 left-0 w-full lg:w-7/12 bg-gradient-to-r from-black/55 to-transparent pointer-events-none" aria-hidden />
 
-      <div className="relative z-10 hpg-container px-4 sm:px-6 pt-24 pb-6 sm:pt-28 sm:pb-14 lg:pt-32 lg:pb-24">
+      <div className="relative z-10 hpg-container px-4 sm:px-6 pt-32 pb-6 sm:pt-32 sm:pb-14 lg:pt-36 lg:pb-24">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-3 sm:gap-8 items-start">
           <div className="lg:col-span-7 hpg-fadein text-center lg:text-left">
             <h1 className="font-display text-[1.5rem] sm:text-[2.6rem] lg:text-[3.4rem] font-black leading-[1.05] uppercase text-white mb-2 sm:mb-4">
